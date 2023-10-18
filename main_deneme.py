9e7585d2ba95e3c5773fb70cf7dc92d3ae85fdd5
Ahmet Aydoğan <aydogan60ahmet@gmail.com>
2023-10-18 23:19:04 +0300

    main_deneme revision - Cryption func added

--- main_deneme.py
+++ main_deneme.py
@@ -1,11 +1,15 @@
 import tkinter as tk
+import base64
+import os
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from PIL import ImageTk, Image
 from tkinter import messagebox
 
 
-window = tk.Tk()
-
 # window settings
+window = tk.Tk()
 SCREEN_WIDTH = window.winfo_screenwidth()
 SCREEN_HEIGHT = window.winfo_screenheight()
 window_width = 400
@@ -19,27 +23,6 @@ window.resizable(False, False)
 style = ("Calibri", 14, "normal")
 widget_width = 30
 img = ImageTk.PhotoImage(Image.open("pngwing.com.png").resize((150, 150)))
-
-
-# Defines
-def save_and_encrypt():
-    if (widgets.title_entry.get() == "" or widgets.secret_entry.get("1.0", 'end-1c') == "" or widgets.master_key_entry.get() == ""):
-        messagebox.showerror("Değerleri Düzgün Giriniz!", "Lütfen değerleri düzgün giriniz.")
-    else:
-        text_title = widgets.title_entry.get()
-        text_content = widgets.secret_entry.get("1.0", 'end-1c')
-        text_master_key = widgets.master_key_entry.get()
-        with open("my_secret.txt", mode='a') as f:
-            pass
-
-
-
-# Image Placement
-def place_image():
-    label = tk.Label(window)
-    label.config(image = img)
-    label.pack()
-    print("sa")
 
 
 class Widgets():
@@ -59,7 +42,8 @@ class Widgets():
         self.secret_entry.config(font=style, height=5, width=widget_width)
         self.master_key_entry.config(font=style, width=widget_width)
 
-        self.save_encrypt_button = tk.Button(text="Save & Encrypt", font=style, width=widget_width, bg="#7f8fa6", fg="#f5f6fa", command=save_and_encrypt)
+        self.save_encrypt_button = tk.Button(text="Save & Encrypt", font=style, width=widget_width, bg="#7f8fa6",
+                                             fg="#f5f6fa", command=save_and_encrypt)
         self.decrypt_button = tk.Button(text="Decrypt", font=style, width=widget_width, bg="#7f8fa6", fg="#f5f6fa")
 
         self.title_label.pack()
@@ -74,7 +58,48 @@ class Widgets():
         self.save_encrypt_button.pack(pady=(0, 20))
         self.decrypt_button.pack()
 
+class CryptographyFunctions():
+    salt = os.urandom(16) # bytes tipinde bir değişken oluştur
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=480000,
+    )
+
+    def cryption_func(self, master_key: str, content: str):
+        master_key = bytes(master_key, 'utf-8')
+        key = base64.urlsafe_b64encode(self.kdf.derive(master_key))
+        fernet = Fernet(key)
+        encrypt_text = bytes(content, 'utf-8')
+        token = fernet.encrypt(encrypt_text)
+        return token
+
+
+# Defines
+def save_and_encrypt():
+    if (widgets.title_entry.get() == "" or widgets.secret_entry.get("1.0", 'end-1c') == ""
+            or widgets.master_key_entry.get() == ""):
+        messagebox.showerror("Değerleri Düzgün Giriniz!", "Lütfen değerleri düzgün giriniz.")
+    else:
+        text_title = widgets.title_entry.get()
+        text_content = widgets.secret_entry.get("1.0", 'end-1c')
+        text_master_key = widgets.master_key_entry.get()
+        with open("my_secret.txt", mode='a') as f:
+            result = str(cryptographyFunc.cryption_func(text_master_key, text_content))
+            added_text = f"{text_title}" + f"\n" + f"{result}"
+            f.write(added_text)
+
+# Image Placement
+def place_image():
+    label = tk.Label(window)
+    label.config(image=img)
+    label.pack()
+    print("sa")
+
 widgets = Widgets()
+cryptographyFunc = CryptographyFunctions()
 
-
+# b'gAAAAABlMDthmR0xgK6VBZAxrBuo2yL2i6yICHDd8OO2kh_rQFX-gU1aQD7zEGLvMrPPJyaltFd4Z-5Jc04-tb5cfYT0X_-4eQ=='
+# ahmet123
 window.mainloop()
